68bf8b4d371cecd0662475c4f2ff336611d10ed3
PikaaVibes <schiesserloic123@gmail.com>
2024-11-12 14:07:28 +0100

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -15,4 +15,26 @@ double _mul(double a, double b) {
 double _div(double a, double b) {
 	return a/b;
 }
-	
+
+#include "calculator.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int divi(int a, int b) {
+    return (b != 0) ? a / b : 0; // Evite la division par zéro
+}
+
+// 👇 Voici ta nouvelle fonction
+int car(int a) {
+    return a * a; // Calcul du carré
+}
