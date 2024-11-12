3cd431fbe163b4dff92278aa30dcc093a841312f
PikaaVibes <schiesserloic123@gmail.com>
2024-11-12 14:16:51 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,14 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
-
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int divi(int a, int b);
 int car(int a);
 #endif
